293a266e168eb9995066eba5d4a79ec668666290
xnomle <Nick.f@hotmail.co.uk>
2026-01-02 13:51:56 +1100

    add: build

---
+++ terraform/build/modules/ec2.py
@@ -0,0 +1,79 @@
+import boto3
+
+def get_ec2_details(event):
+
+    # 1. Grab standard details 
+    account_number = event.get('account')
+    region         = event.get('region')
+    alert_time     = event.get('time')
+
+    # 2. Grab details of event
+    details = event.get('detail', {})
+    
+    # EventBridge 'resources' is a list of ARNs
+    resources     = event.get("resources", [])
+    # resource_arn  = resources[0] if resources else "Unknown" # Optional: if you want the full ARN
+
+    instance_id   = details.get('instance-id')
+    state         = details.get('state')
+
+    
+    ec2_client = boto3.client('ec2', region_name=region)
+    
+    try:
+        response = ec2_client.describe_instances(
+            InstanceIds=[instance_id]
+        )
+
+        instance_data = response['Reservations'][0]['Instances'][0]
+        # Get the reason, default to 'Unknown' if missing
+        reason = instance_data.get('StateTransitionReason', 'Unknown')
+
+        # 4. Construct Blocks with F-Strings
+        message_blocks = [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "🚨 EC2 Instance State Change"
+                }
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Account Number:*\n{account_number}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Region:*\n{region}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Alert Time:*\n{alert_time}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Resource:*\nEC2"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Instance ID:*\n{instance_id}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*New State:*\n{state}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Reason:*\n{reason}"
+                    }
+                ]
+            }
+        ]
+
+        return message_blocks
+
+    except Exception as e:
+        return f"Error getting details for {instance_id}. Error: {str(e)}"
